20c33abc11bfb3e26aa9ce52b8251052f7131bb8
droidsam1 <droidsam@gmail.com>
2018-08-22 12:00:28 +0200

    maven archetype

---
+++ lessons/lesson 4/missinginteger/src/main/java/noobsAcademy/missinginteger/Solution.java
@@ -0,0 +1,14 @@
+package noobsAcademy.missinginteger;
+
+/**
+ * Write a function:
+ * 
+ * that, given an array A of N integers, returns the smallest positive integer
+ * (greater than 0) that does not occur in A.
+ *
+ */
+public class Solution {
+    public static int solution(int[] A) {
+	throw new UnsupportedOperationException("Not Implemented");
+    }
+}
