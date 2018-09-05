f55d3f8f72943de571c6db9f32823b85d1e9371f
droidsam1 <droidsam@gmail.com>
2018-09-05 18:16:47 +0200

    lesson 5
    MinAvgTwoSlice

---
+++ lessons/lesson5/minavgtwoslice/src/test/java/noobsAcademy/minavgtwoslice/AppTest.java
@@ -0,0 +1,34 @@
+package noobsAcademy.minavgtwoslice;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName
+     *            name of the test case
+     */
+    public AppTest(String testName) {
+	super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+	return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testExample() {
+	assertEquals(1, Solution.solution(new int[] { 4, 2, 2, 5, 1, 5, 8 }));
+    }
+}
